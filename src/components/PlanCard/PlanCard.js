4500fd6f8a958ebd18c2ef6c2e4317c56ee5cfdd
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-11-29 17:23:07 -0600

    reconfigured bottom nav to a func comp now uses hooks

--- src/components/PlanCard/PlanCard.js
+++ src/components/PlanCard/PlanCard.js
@@ -7,8 +7,8 @@ export const PlanCard = (props) => {
 
     <article className="flex flex-col shadow-2xl my-4">
       {/* Article Image */}
-      <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" className="object-cover rounded-t-lg" alt="post" />
-      <div className="bg-deep-blue flex flex-col justify-start p-6 rounded-b-lg text-white-lilac text-sans">
+      <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" className="object-cover" alt="post" />
+      <div className="bg-deep-blue flex flex-col justify-start p-6 text-white-lilac text-sans">
         <Link to="#" className="text-2xl font-medium hover:text-biloba-violet pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</Link>
         <Link to="#" className="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</Link>
         <button to="#" className="font-sans font-medium text-base text-deep-blue bg-biloba-violet
