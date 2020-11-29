873731be9606f23e1c169d3de1f54fdc9d761601
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-11-28 21:57:38 -0600

    plan card changes

--- src/components/PlanCard/PlanCard.js
+++ src/components/PlanCard/PlanCard.js
@@ -5,19 +5,14 @@ import { Link } from 'react-router-dom'
 export const PlanCard = (props) => {
   return (
 
-    <article className="flex flex-col shadow my-4">
+    <article className="flex flex-col shadow-2xl my-4">
       {/* Article Image */}
-      <Link to="#" className="hover:opacity-75">
-        <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" alt="post" />
-      </Link>
-      <div className="bg-white flex flex-col justify-start p-6">
-        <Link to="#" className="text-blue-700 text-sm font-bold uppercase pb-4">Technology</Link>
-        <Link to="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</Link>
-        <p to="#" className="text-sm pb-3">
-          By <Link to="#" className="font-semibold hover:text-gray-800">David Grzyb</Link>, Published on April 25th, 2020
-      </p>
+      <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" className="object-cover rounded-t-lg" alt="post" />
+      <div className="bg-deep-blue flex flex-col justify-start p-6 rounded-b-lg text-white-lilac text-sans">
+        <Link to="#" className="text-2xl font-medium hover:text-biloba-violet pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</Link>
         <Link to="#" className="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</Link>
-        <Link to="#" className="uppercase text-gray-800 hover:text-black">Continue Reading <i className="fas fa-arrow-right" /></Link>
+        <button to="#" className="font-sans font-medium text-base text-deep-blue bg-biloba-violet
+        hover:text-white-lilac hover:bg-electric-violet py-3 px-3 mx-auto w-4/12 rounded-full ">Read More</button>
       </div>
     </article>
   )
--- src/components/Plans/Plans.js
+++ src/components/Plans/Plans.js
@@ -4,7 +4,9 @@ import { PlanCard } from '../PlanCard/PlanCard'
 
 const Plans = () => {
   return (
-    <div className="w-full md:w-2/3 flex flex-col items-center my-16 px-3">
+    <div className="w-full md:w-2/3 flex flex-col justify-center content-center my-16 mx-auto px-3">
+      <h1 className="text-4xl text-white-lilac text-center font-sans font-medium mt-3 mb-8">Types of Fasts</h1>
+      <PlanCard />
       <PlanCard />
     </div>
   )
