b0ef32e4f5021c058bd64e814baa8863a29b4b71
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-12-02 14:19:13 -0600

    modal for adding custom fast

---
+++ src/components/Fasts/FastFrom/FastModal.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import Modal from 'react-modal'
+
+import './modal.css'
+
+
+Modal.setAppElement('#root')
+
+const FastModal = () => {
+  const [modalIsOpen, setIsOpen] = useState(false)
+
+  const openModal = () => {
+    setIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <button onClick={openModal} className="flex justify-center font-sans font-medium text-base text-biloba-violet border border-biloba-violet hover:bg-electric-violet hover:border-electric-violet py-2 mx-auto w-1/4 rounded-full mb-8">
+        <svg xmlns="http://www.w3.org/2000/svg" width="14.74" height="14.74" viewBox="0 0 14.74 14.74" className="self-center pr-1">
+          <g id="Icon_feather-plus" data-name="Icon feather-plus" transform="translate(-6 -6)">
+            <path id="Path_6" data-name="Path 6" d="M18,7.5V19.24" transform="translate(-4.63)" fill="none" stroke="#B4B7EE" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" />
+            <path id="Path_7" data-name="Path 7" d="M7.5,18H19.24" transform="translate(0 -4.63)" fill="none" stroke="#B4B7EE" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" />
+          </g>
+        </svg>
+  Custom
+    </button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Custom Fast"
+        className="Modal"
+      // overlayClassName="Overlay"
+      >
+        <h1>Holla</h1>
+      </Modal>
+    </div>
+  )
+}
+
+export default FastModal
--- src/components/Fasts/Fasts.js
+++ src/components/Fasts/Fasts.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Modal from 'react-modal'
+import FastModal from './FastFrom/FastModal'
 
 import { FastCard } from './FastCard'
 
@@ -7,16 +7,7 @@ const Fasts = () => {
   return (
     <div className="w-full md:w-2/3 flex flex-col justify-center content-center my-16 mx-auto px-3">
       <h1 className="text-4xl text-white-lilac text-center font-sans font-medium mt-3">Types of Fasts</h1>
-      <button to="#" className="flex justify-center font-sans font-medium text-base text-biloba-violet border border-biloba-violet hover:bg-electric-violet hover:border-electric-violet py-2 mx-auto w-1/4 rounded-full mb-8">
-        <svg xmlns="http://www.w3.org/2000/svg" width="14.74" height="14.74" viewBox="0 0 14.74 14.74" className="self-center pr-1">
-          <g id="Icon_feather-plus" data-name="Icon feather-plus" transform="translate(-6 -6)">
-            <path id="Path_6" data-name="Path 6" d="M18,7.5V19.24" transform="translate(-4.63)" fill="none" stroke="#B4B7EE" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" />
-            <path id="Path_7" data-name="Path 7" d="M7.5,18H19.24" transform="translate(0 -4.63)" fill="none" stroke="#B4B7EE" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" />
-          </g>
-        </svg>
-
-        Custom
-      </button>
+      <FastModal />
 
 
       <FastCard />
