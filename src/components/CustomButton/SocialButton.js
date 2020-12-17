67074024e35aa1c45a11e0137d88210a5dbe41cd
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-12-17 12:47:58 -0600

    consolidate social buttons to a dynamic component

---
+++ src/components/CustomButton/SocialButton.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FaFacebookF, FaTwitter } from 'react-icons/fa'
+import { FcGoogle } from 'react-icons/fc'
+
+const Components = {
+  google: FcGoogle,
+  facebook: FaFacebookF,
+  twitter: FaTwitter
+}
+
+const SocialButton = ({ tag, color }) => {
+  const TagName = Components[tag || '']
+
+  return (
+    <button className="p-3 bg-transparent border-0 opacity-5 float-right text-2xl rounded-full bg-white leading-none font-semibold outline-none focus:outline-none shadow">
+      <TagName className={color} />
+    </button>
+  )
+
+}
+
+export default SocialButton
