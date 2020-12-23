a85f5e33a1d888e63b5ee68d91b80c19d25f8f97
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-12-23 17:30:26 -0600

    firebase file rename

--- src/fbConfig.js
+++ src/fbConfig.js
@@ -20,6 +20,6 @@ var config = {
 firebase.initializeApp(config);
 firebase.analytics();
 firebase.firestore().settings({ timestampsInSnapshots: true })
-firebase.firestore()
+// firebase.firestore()
 
 export default firebase
