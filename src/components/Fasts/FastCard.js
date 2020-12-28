d1134afd43cb196e4a705bb0e9bd6cbd3c52279c
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-12-28 16:41:29 -0600

    temporary conditional for card image

--- src/components/Fasts/FastCard.js
+++ src/components/Fasts/FastCard.js
@@ -7,7 +7,10 @@ const FastCard = ({ fast }) => {
 
   return (
     <article className="flex flex-col shadow-2xl my-4" >
-      < img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" className="object-cover" alt="post" />
+      {fast.imageURL
+        ? < img src={fast.imageURL} className="object-cover" alt="post" />
+        : < img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" className="object-cover" alt="post" />}
+
       <div className="bg-deep-blue flex flex-col justify-start p-6 text-white-lilac text-sans">
 
         <p className="text-2xl font-medium hover:text-biloba-violet pb-1">{fast.title}</p>
