d97af636577121dad50321c14d76634f4e4dbddd
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-12-29 14:02:10 -0600

    card edit

--- src/components/Fasts/FastCard.js
+++ src/components/Fasts/FastCard.js
@@ -3,24 +3,28 @@ import * as dayjs from 'dayjs'
 
 import CustomButton from '../CustomButton/CustomButton'
 
+import './FastCard.css'
+
 const FastCard = ({ fast }) => {
 
   return (
-    <article className="flex flex-col shadow-2xl my-4" >
-      {fast.imageURL
-        ? < img src={fast.imageURL} className="object-cover" alt="post" />
-        : < img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1" className="object-cover" alt="post" />}
-
-      <div className="bg-deep-blue flex flex-col justify-start p-6 text-white-lilac text-sans">
-
-        <p className="text-2xl font-medium hover:text-biloba-violet pb-1">{fast.title}</p>
-
-        <div className="text-sm" dangerouslySetInnerHTML={{ __html: fast.body }}></div>
-        <div className="m-1 text-xs text-gray-500">
-          {dayjs(fast.createdAt.toDate()).format('MMM DD, YYYY')}
+    <>
+      <div className="h-48 lg:h-auto lg:w-48 md:h-auto md:w-40 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden object-cover" style={{ backgroundImage: `url(${fast.imageURL ? fast.imageURL : 'https://source.unsplash.com/collection/1346951/1000x500?sig=1'})`, backgroundPosition: 'center' }} title={fast.title}>
+      </div>
+      <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal w-full">
+        <div className="mb-8">
+          <div className="font-medium hover:text-biloba-violet text-xl mb-2">{fast.title}</div>
+          <div className="ellipses text-gray-700 text-sm" dangerouslySetInnerHTML={{ __html: fast.body }}></div>
+        </div>
+        <div className="flex items-center md:flex-col lg:flex-row">
+          <img className="w-10 h-10 rounded-full mr-4" src="https://i.pravatar.cc/300" alt="Avatar of Writer" />
+          <div className="text-sm">
+            <p className="text-gray-900 leading-none">John Smith</p>
+            <p className="text-gray-600">{dayjs(fast.createdAt.toDate()).format('MMM DD, YYYY')}</p>
+          </div>
         </div>
       </div>
-    </article >
+    </>
   )
 }
 
