72e29698efcb2e1397a957e5e14ea84823c8365a
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2021-02-01 22:34:28 -0600

    import and recipe page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link, withRouter } from "react-router-dom"
 
 import NavbarLink from './NavbarLink'
+import UserMenuNav from './UserMenuNav'
 
 // ICONS
 import { BiBell, BiMenuAltRight, BiX } from 'react-icons/bi'
