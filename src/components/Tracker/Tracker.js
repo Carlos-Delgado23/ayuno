0018b42dde3ba0559e3842951d4298a6f7d9e1bf
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2021-03-21 13:05:45 -0500

    inital tracker page start

--- src/components/Tracker/Tracker.js
+++ src/components/Tracker/Tracker.js
@@ -1,11 +1,45 @@
 import React from 'react'
+import { Link, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Tracker = () => {
-  return (
-    <div>
-      Tracker
-    </div>
-  )
+class Tracker extends React.Component {
+  render() {
+    return (
+      <div className="flex justify-center content-center h-screen">
+        <div className="w-5/6 my-20 max-w-xl p-6 bg-white-lilac rounded-xl flex flex-col">
+
+          <div>
+            Countdown
+          </div>
+
+          <div>
+            <div>start</div>
+            <div>end</div>
+          </div>
+
+          <div>Progress Circle</div>
+
+          <div>Verse</div>
+
+          <button>Finish/Start Fast</button>
+
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Tracker
+const mapStateToProps = state => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tracker)
+
